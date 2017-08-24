ed464764d35a4855e4905589ecbcc836113ab75c
davebur26 <davebur26@gmail.com>
2017-08-24 22:17:52 +0100

    add 'all' methods to artist class

--- models/artist.rb
+++ models/artist.rb
@@ -7,13 +7,23 @@ class Artist
 
 
   def initialize( details )
-    @id = options['id'].to_i if options['id']
-    @name = options['name']
+    @id = details['id'].to_i if details['id']
+    @name = details['name']
   end
 
   def save()
-    sql = "INSERT INTO artists(name) VALUES ($1,$2) RETURNING id"
+    sql = "INSERT INTO artists(name) VALUES ($1) RETURNING id"
     values = [@name]
     results = SqlRunner.run(sql, values)
     @id = results.first()['id'].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM artists"
+    values = []
+    results = SqlRunner.run( sql, values )
+    return results.map{ |artist| Artist.new() }
+  end
+
+
+end
