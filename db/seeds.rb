df52d10b03583c83a8afe40122776bc3646e1b59
davebur26 <davebur26@gmail.com>
2017-08-24 20:29:04 +0100

    create artist instances and save to db

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+require_relative( '../models/artist.rb')
+require( 'pry-byebug' )
+
+artist1 = Artist.new({"name" => "The Foals"})
+artist2 = Artist.new({"name" => "Foo Fighters"})
+artist3 = Artist.new({"name" => "Kasabian"})
+artist4 = Artist.new({"name" => "The Red Hot Chili Peppers"})
+artist5 = Artist.new({"name" => "Arctic Monkeys"})
+artist6 = Artist.new({"name" => "Stereophonics"})
+
+artist1.save()
+artist2.save()
+artist3.save()
+artist4.save()
+artist5.save()
+artist6.save()
