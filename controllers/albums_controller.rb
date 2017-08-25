0a465f14148cb8c1b6fd84570323f9968d0a5824
davebur26 <davebur26@gmail.com>
2017-08-25 17:01:06 +0100

    add artist name instead of artist id in album info

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -1,13 +1,35 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/album.rb' )
+require_relative( '../models/artist.rb' )
 
 # INDEX
 get '/albums' do
   @albums = Album.all()
+  @artists = Artist.all
   erb( :"albums/index")
 end
 
+# new
+get '/album/new' do
+  @albums = Album.all
+  @artists = Artist.all
+  erb( :"albums/new" )
+end
+
+# CREATE
+post '/albums' do
+  album = Album.new(params)
+  album.save
+  redirect to ("/albums")
+end
+
+
+# SHOW
+get '/albums/:id' do
+  @album = Album.find(params[:id])
+  erb( :"albums/show" )
+end
 
 # DESTROY
 post '/albums/:id/delete' do
--- models/album.rb
+++ models/album.rb
@@ -1,15 +1,18 @@
 require_relative( '../db/sql_runner' )
+require_relative( './artist.rb' )
 
 class Album
 
   attr_accessor(:title, :artist_id, :quantity)
-  attr_reader(:name)
+  attr_reader(:id, :artist_name)
 
   def initialize( details )
     @id = details['id'].to_i if details['id']
     @title = details['title']
     @artist_id = details['artist_id']
     @quantity = details['quantity']
+    @artist_name = Artist.find(artist_id).name
+
   end
 
   def save()
@@ -29,6 +32,14 @@ class Album
     return results.map{ |album| Album.new(album) }
   end
 
+  def self.find(id)
+    sql = "SELECT * FROM albums WHERE id = $1"
+    values = [id]
+    album = SqlRunner.run(sql,values)
+    result = Album.new(album[0])
+    return result
+  end
+
   def delete()
     sql = "DELETE FROM albums WHERE id = $1"
     values = [@id]
@@ -39,6 +50,16 @@ class Album
     sql = "DELETE FROM albums;"
     values = []
     SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE albums SET (title = $1, artist_id = $2, quantity = $3 WHERE id = $4"
+    values = [@title, @artist_id, @quantity, @id]
+    SqlRunner.run( sql, values )
+  end
+
+  def artist()
+
   end
 
 end
