5fcade7da9c173e53fe867a472a97de8eedb71b9
davebur26 <davebur26@gmail.com>
2017-08-25 15:03:16 +0100

    add require relative for albums controller

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( 'controllers/artists_controller')
+require_relative( 'controllers/albums_controller')
 
 get '/' do
   erb( :index )
