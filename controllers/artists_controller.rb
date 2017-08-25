ad43c6ec7322becbaec72ec4bdf0b06a03067c5f
davebur26 <davebur26@gmail.com>
2017-08-25 13:45:28 +0100

    add notes

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -2,38 +2,45 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/artist.rb' )
 
+# INDEX
 get '/artists' do
   @artists = Artist.all()
   erb ( :"artists/index")
 end
 
+# NEW
 get '/artist/new' do
   @artists = Artist.all
   erb( :"artists/new" )
 end
 
+# CREATE
 post '/artists' do
   artist = Artist.new(params)
   artist.save
   redirect to ("/artists")
 end
 
+# SHOW
 get '/artists/:id' do
   @artist = Artist.find(params[:id])
   erb( :"artists/show" )
 end
 
+# DESTROY
 post '/artists/:id/delete' do
   artist = Artist.find(params[:id])
   artist.delete()
   redirect to ("/artists")
 end
 
+# EDIT
 get '/artists/:id/edit' do
   @artist = Artist.find( params[:id] )
   erb( :"artists/edit" )
 end
 
+# UPDATE
 post '/artists/:id' do
   Artist.new( params ).update
   redirect to '/artists'
