d51b899b434257faa6249fbd86d4ee19a3bf8269
davebur26 <davebur26@gmail.com>
2017-08-25 17:04:02 +0100

    updated bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require_relative( '../models/artist.rb');
 require_relative( '../models/album.rb');
 require( 'pry-byebug' )
 
+Album.delete_all()
 Artist.delete_all()
 
 artist1 = Artist.new({"name" => "The Foals"})
