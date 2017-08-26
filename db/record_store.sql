4ffa61ad3007ba479ec69f8a5bf1fe1863d57bda
davebur26 <davebur26@gmail.com>
2017-08-26 08:08:53 +0100

    add genre table and additional info to albums and artists

--- db/record_store.sql
+++ db/record_store.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS albums;
 DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS genres;
 
 
 CREATE TABLE artists (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  url VARCHAR(255)
+);
+
+CREATE TABLE genres (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
 );
@@ -11,5 +18,10 @@ CREATE TABLE albums (
   id SERIAL8 PRIMARY KEY,
   title VARCHAR(255),
   artist_id INT8 REFERENCES artists(id),
-  quantity VARCHAR(255)
+  genre_id INT8 REFERENCES genres(id),
+  quantity VARCHAR(255),
+  buy_cost INT,
+  sell_price INT,
+  last_bought INT,
+  url VARCHAR(255)
 );
