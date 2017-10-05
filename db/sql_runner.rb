41f1ebcca84a63609d72a92dad21f677f6cae975
davebur26 <davebur26@gmail.com>
2017-10-05 13:47:00 +0100

    connect sql runner to haroku settings

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,13 @@ class SqlRunner
 
   def self.run( sql, values )
     begin
-      db = PG.connect({ dbname: 'record_store', host: 'localhost' })
+      db = PG.connect({
+        dbname: 'd9p85v8dotojtt',
+        host: 'ec2-54-225-237-64.compute-1.amazonaws.com'
+        port: 5432,
+        user: 'fwjznujpjefxlb',
+        password: '231a9ef429130c022a343623f63cb41801289bd061adc24ac626103dce83e671'
+        })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
