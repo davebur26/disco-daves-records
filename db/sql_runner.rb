8e88a7ec2aaf5f98b69972e01b689d677acee006
davebur26 <davebur26@gmail.com>
2017-10-05 13:53:39 +0100

    connect sql runner to haroku settings

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -6,7 +6,7 @@ class SqlRunner
     begin
       db = PG.connect({
         dbname: 'd9p85v8dotojtt',
-        host: 'ec2-54-225-237-64.compute-1.amazonaws.com'
+        host: 'ec2-54-225-237-64.compute-1.amazonaws.com',
         port: 5432,
         user: 'fwjznujpjefxlb',
         password: '231a9ef429130c022a343623f63cb41801289bd061adc24ac626103dce83e671'
